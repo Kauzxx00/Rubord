db82f9f629d3e8567f71d025bfd2afc024943426
Kauzxx00 <ed3ard0k13@gmail.com>
2026-01-12 23:08:22 -0300

    Refactor Intents module to enhance functionality and add new methods for intent management

--- lib/rubord/models/flags.rb
+++ lib/rubord/models/flags.rb
@@ -67,8 +67,165 @@ module Rubord
       auto_moderation_execution:     1 << 21
     }.freeze
 
-    def self.combine(*intents)
-      intents.map { |i| FLAGS[i] }.compact.reduce(0, :|)
+    
+
+    class << self
+      def [](intent_name)
+        FLAGS[intent_name.to_sym] if intent_name
+      end
+
+      def all = FLAGS.values.reduce(0, :|);
+
+      def exists?(intent_name)
+        FLAGS.key?(intent_name.to_sym)
+      end
+
+      def names
+        FLAGS.keys
+      end
+
+      def values
+        FLAGS.values
+      end
+
+      def combine(*intents)
+        intents.flatten.reduce(0) do |sum, intent|
+          case intent
+          when Integer
+            sum | intent
+          when Symbol, String
+            value = FLAGS[intent.to_sym]
+            unless value
+              warn "[Rubord:Intents] Unknown intent: #{intent.inspect}"
+              next sum
+            end
+            sum | value
+          else
+            warn "[Rubord:Intents] Invalid intent type: #{intent.class}"
+            sum
+          end
+        end
+      end
+
+      def guilds
+        FLAGS[:guilds]
+      end
+
+      def guild_members
+        FLAGS[:guild_members]
+      end
+
+      def guild_bans
+        FLAGS[:guild_bans]
+      end
+
+      def guild_emojis
+        FLAGS[:guild_emojis]
+      end
+
+      def guild_integrations
+        FLAGS[:guild_integrations]
+      end
+
+      def guild_webhooks
+        FLAGS[:guild_webhooks]
+      end
+
+      def guild_invites
+        FLAGS[:guild_invites]
+      end
+
+      def guild_voice_states
+        FLAGS[:guild_voice_states]
+      end
+
+      def guild_presences
+        FLAGS[:guild_presences]
+      end
+
+      def guild_messages
+        FLAGS[:guild_messages]
+      end
+
+      def guild_message_reactions
+        FLAGS[:guild_message_reactions]
+      end
+
+      def guild_message_typing
+        FLAGS[:guild_message_typing]
+      end
+
+      def direct_messages
+        FLAGS[:direct_messages]
+      end
+
+      def direct_message_reactions
+        FLAGS[:direct_message_reactions]
+      end
+
+      def direct_message_typing
+        FLAGS[:direct_message_typing]
+      end
+
+      def message_content
+        FLAGS[:message_content]
+      end
+
+      def guild_scheduled_events
+        FLAGS[:guild_scheduled_events]
+      end
+
+      def auto_moderation_configuration
+        FLAGS[:auto_moderation_configuration]
+      end
+
+      def auto_moderation_execution
+        FLAGS[:auto_moderation_execution]
+      end
+
+      def default
+        combine(
+          :guilds,
+          :guild_members,
+          :guild_messages,
+          :guild_message_reactions,
+          :direct_messages,
+          :direct_message_reactions
+        )
+      end
+
+      def privileged
+        combine(
+          :guild_members,
+          :guild_presences,
+          :message_content
+        )
+      end
+
+      def include?(intent_value, intent_name)
+        intent_value & self[intent_name] != 0
+      end
+
+      def method_missing(name, *args)
+        if FLAGS.key?(name)
+          FLAGS[name]
+        elsif name.to_s.end_with?('?') && FLAGS.key?(name.to_s[0...-1].to_sym)
+          intent_name = name.to_s[0...-1].to_sym
+          if args.first.is_a?(Integer)
+            args.first & FLAGS[intent_name] != 0
+          else
+            super
+          end
+        else
+          super
+        end
+      end
+
+      def respond_to_missing?(name, include_private = false)
+        FLAGS.key?(name) || 
+        (name.to_s.end_with?('?') && FLAGS.key?(name.to_s[0...-1].to_sym)) ||
+        super
+      end
     end
   end
 
