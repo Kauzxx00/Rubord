e6a80eee97b007fdcd8f9edde100bb2b9b1b7314
Kauzxx00 <ed3ard0k13@gmail.com>
2026-02-06 16:56:29 -0300

    Refactor Text component to accept variable content arguments and join them for output

--- lib/rubord/components/containers/text.rb
+++ lib/rubord/components/containers/text.rb
@@ -3,7 +3,7 @@ require_relative "base"
 module Rubord
   module Components
     class Text < BaseComponent
-      def initialize(content)
+      def initialize(*content)
         super(10)
         @content = content
       end
@@ -11,13 +11,13 @@ module Rubord
       def to_h
         {
           type: @type,
-          content: @content
+          content: @content.join("\n")
         }
       end
     end
   end
 
-  def self.Text(content)
+  def self.Text(*content)
     Components::Text.new(content)
   end
 end
