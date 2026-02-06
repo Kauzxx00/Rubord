3b071a1fb62d4c1d59ac52c253c31f33fa56c12d
Kauzxx00 <ed3ard0k13@gmail.com>
2026-02-06 16:57:39 -0300

    Refactor CommandRegistry to include logCommands parameter in register method for improved logging control

--- lib/rubord/models/commands/registry.rb
+++ lib/rubord/models/commands/registry.rb
@@ -2,21 +2,19 @@ module Rubord
   class CommandRegistry
     def initialize
       @commands = {}
-      @command_classes = {}
     end
 
-    def register(klass, client)
+    def register(klass, client, logCommands = true)
       name = klass.command_name
       raise "Command missing name" unless name
       
-      @command_classes[name] = klass
       @commands[name] = klass.new(client)
       
-      Rubord::Logger.success "[Rubord:Commands] Registered command: #{name}"
+      Rubord::Logger.success "[Rubord:Commands] Registered command: #{name}" if logCommands
     end
 
     def get(name)
-      @commands[name]
+      @commands.find{ |_, c| c.name == name || c.aliases.include?(name)}&.last
     end
 
     def all
